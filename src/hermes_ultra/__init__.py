46c1072df13b136831899c8d654fb7a87fa0f052
hermes-max100 <dab-risk-portfolio@duck.com>
2026-08-24 04:09:02 -0700

    feat: expose Agent-Reach adapter

---
+++ src/hermes_ultra/__init__.py
@@ -0,0 +1,13 @@
+from .agent_reach import (
+    SUPPORTED_CHANNELS,
+    AgentReachAdapter,
+    AgentReachError,
+    AgentReachResult,
+)
+
+__all__ = [
+    "SUPPORTED_CHANNELS",
+    "AgentReachAdapter",
+    "AgentReachError",
+    "AgentReachResult",
+]
